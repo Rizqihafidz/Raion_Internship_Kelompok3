8f83b0e5a080d6254e7c56d6d796e90803726695
EricoSilitonga <81163445+EricoSilitonga@users.noreply.github.com>
2021-04-01 19:43:09 +0700

    Add files via upload

---
+++ Assets/Scripts/Enemi.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemi : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    [SerializeField]
+    private PlayerMovement _player;
+    public bool _moveRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (_moveRight)
+        {
+            transform.Translate(Vector2.right * _speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * _speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _player.Damage();
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Turn"))
+        {
+            if (_moveRight) {
+                _moveRight = false;
+            }else
+                {
+                 _moveRight = true;
+                }
+            }
+        
+    }
+
+}
+
